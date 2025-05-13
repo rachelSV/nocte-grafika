d23bee3dbe2fec5647bd60e6ab366d25700a67ad
rachelSV <lestat.elijah31@gmail.com>
2025-05-13 17:59:49 +0200

    feat: :sparkles: création du nouveau composant card-title-text et 1er essai dans app-component en brut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { CardTitleTextImgButtonComponent } from './components/card-title-text-img-button/card-title-text-img-button.component';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,21 @@ import { FooterComponent } from './layouts/footer/footer.component';
     RouterOutlet, 
     CommonModule,
     HeaderComponent,
-    FooterComponent  
+    FooterComponent,
+    CardTitleTextImgButtonComponent  
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'nocte-grafika';
+
+  aboutMeList = [
+  {
+    title: 'About me ...',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie...`,
+    imgPath: '/assets/images/circle.png',
+    imgAlt: 'Nocte Logo'
+  }
+];
 }
--- src/app/components/card-title-text-img-button/card-title-text-img-button.component.ts
+++ src/app/components/card-title-text-img-button/card-title-text-img-button.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-title-text-img-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-title-text-img-button.component.html',
   styleUrl: './card-title-text-img-button.component.scss'
 })
 export class CardTitleTextImgButtonComponent {
 
+  @Input() title: string = '';
+  @Input() description: string = '';
+
+  @Input() containerClass: string = 'glassmorphism p-6 sm:p-8  w-full rounded-lg shadow-lg';
+  @Input() titleClass: string = 'text-3xl sm:text-4xl text-primary font-fraktur mb-4 uppercase';
+  @Input() descriptionClass: string = 'text-primary text-sm sm:text-base leading-relaxed mb-6';
 }
